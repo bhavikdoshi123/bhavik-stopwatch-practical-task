b0b800ae37694826df4164119770f1b72c1c8145
bhavik <doshibhavik318@gmail.com>
2024-07-23 23:29:19 +0530

    first commit

---
+++ src/Pages/App.js
@@ -0,0 +1,14 @@
+import "../assets/scss/main.scss";
+import Stopwatch from "./Eventbar/Stopwatch";
+
+function App() {
+  return (
+      <>
+      <div className="App">
+      <Stopwatch/>
+     </div>
+     </>
+  );
+}
+
+export default App;
